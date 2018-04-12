db0b8e586035599978b8d0b0f091932f094417b2
asepridwan1989 <aseprmail@gmail.com>
2018-04-12 15:10:22 +0700

    done

--- beli.js
+++ beli.js
@@ -1,15 +1,19 @@
 function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    // console.log(obj)
+    // console.log(uang ,obj.harga)
+    // console.log(uang > obj.harga)
+
+    if (uang > obj.harga) {
+      let kembalian = uang - obj.harga
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+    }if (uang < obj.harga) {
+      // let sisaUang = uang
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
+      cb(uang)
     }
   }, obj.waktu);
 }
-
 module.exports = beli;
---
+++ index.js
@@ -0,0 +1,28 @@
+var beli = require('./beli.js')
+
+class item{
+  constructor(name,harga,waktu){
+    this.item = name
+    this.harga = harga
+    this.waktu = waktu
+  }
+}
+
+let permen = new item('permen',300,200)
+let batu = new item('batu',500,300)
+let gula = new item('gula',350,150)
+let soda = new item('soda',700,200)
+let kacang = new item('kacang',600,200)
+var uang = 1000
+
+beli(uang,permen,kembalianPermen=>{
+    beli(kembalianPermen,batu,kembalianBatu=>{
+        beli(kembalianBatu,gula,kembalianGula=>{
+            beli(kembalianGula,soda,kembalianSoda=>{
+                beli(kembalianSoda,kacang,kembalianKacang=>{
+                    console.log('selesai belanja, sisa uang : ', kembalianKacang)
+                })
+            })
+        })
+    })
+})
