149e34a46a1254cec8adfbe3970070a749f12698
David Joshua <davidjosh_87@yahoo.com>
2018-04-12 15:24:31 +0700

    promise-belanja-afif

--- beli.js
+++ beli.js
@@ -1,13 +1,15 @@
+/*jshint esversion:6*/
 function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
+  console.log(`Saya pergi membeli ${obj.item}`);
   setTimeout(function(){
-    let kembalian = uang - obj.harga
+    let kembalian = uang - obj.harga;
     if (kembalian > 0) {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
+      cb(kembalian);
     }else{
+      kembalian = 0;
       console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      cb(0);
     }
   }, obj.waktu);
 }
